574f8edc32f2e671b965d2c3fbfacd4887885f85
earlgreyhot <jakehamo@gmail.com>
2014-08-25 21:14:51 -0400

    Update problem11.py

--- problem11.py
+++ problem11.py
@@ -22,30 +22,27 @@ def get_matrix(url):
 def max_product(n):
   Matrix = get_matrix('http://projecteuler.net/problem=11')
   product_list=[]
-  for i in range(20):
-    for j in range(20-n):
+  for i in range(20-n):
+    for j in range(20):
       product1 = 1
       product2 = 1
+      for k in range(n):
+        product1 *= int(Matrix[i+k][j])
+        product2 *= int(Matrix[j][i+k])
+    for j in range(20-n):
       product3 = 1
       product4 = 1
       for k in range(n):
-        product1 *= int(Matrix[j+k][i])
-        product2 *= int(Matrix[i][j+k])
-        product3 *= int(Matrix[j+k][j+k])
-        product4 *= int(Matrix[k-j][k-j])
+        product3 *= int(Matrix[i+k][j+k]) 
+        product4 *= int(Matrix[i+k][j-k])
       product_list.extend((product1,product2,product3,product4))
-      
   product_list.sort()
-  print product_list
   return product_list[-1]
  
-print max_product(4)
-pyperclip.copy(max_product(4))
 
+start = time.time()
+answer = max_product(4)
+elapsed = (time.time()-start)
 
-# start = time.time()
-# answer = max_product(4)
-# elapsed = (time.time()-start)
-
-# print "found %s in %s seconds" % (answer,elapsed)
-# pyperclip.copy(answer)
+print "found %s in %s seconds" % (answer,elapsed)
+pyperclip.copy(answer)
